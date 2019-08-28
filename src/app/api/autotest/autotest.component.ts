3740f5a428fe2fc1399752331063490c969cd0a6
decrypt <yashtodkar2@gmail.com>
2019-08-29 01:09:13 +0530

    get request functionality completed.

--- src/app/api/autotest/autotest.component.ts
+++ src/app/api/autotest/autotest.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ApirequestService } from 'src/app/api/apirequest.service';
+import { HttpHeaders } from '@angular/common/http';
+//import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-autotest',
   templateUrl: './autotest.component.html',
   styleUrls: ['./autotest.component.scss']
 })
+
 export class AutotestComponent implements OnInit {
 
-  constructor() { }
+  apiBaseUrl:string = "https://jsonplaceholder.typicode.com/todos?_limit=5";
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-type':'application/json'
+    })
+  }
+
+  constructor(private apiService:ApirequestService) { 
+    
+    this.apiService.get(this.apiBaseUrl).subscribe(i => {
+      console.log(i);
+    });
+
+    this.apiService.get(this.apiBaseUrl,this.httpOptions).subscribe(i => {
+      console.log(i);
+    });
+
+  }
 
   ngOnInit() {
+
   }
 
 }
