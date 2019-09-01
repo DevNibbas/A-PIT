3fa9cd2f766add0a8f88e8fdfe1651a87efe5e81
decrypt <yashtodkar2@gmail.com>
2019-09-01 21:38:41 +0530

    Changes removed for autotestComponent

--- src/app/api/autotest/autotest.component.ts
+++ src/app/api/autotest/autotest.component.ts
@@ -10,31 +10,8 @@ import { HttpHeaders } from '@angular/common/http';
 
 export class AutotestComponent implements OnInit {
 
-  apiBaseUrl:string = "https://jsonplaceholder.typicode.com/todos?_limit=5";
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-type':'application/json'
-    })
-  }
-
   constructor(private apiService:ApirequestService) { 
     
-    // GET request testing done here (URL is required with httpOptions is optional)
-    this.apiService.get(this.apiBaseUrl).subscribe(i => {
-      console.log(i);
-    });
-    this.apiService.get(this.apiBaseUrl,this.httpOptions).subscribe(i => {
-      console.log(i);
-    });
-
-    // POST request testing done here (URL and BODY DATA is required but httpOptions is optional)
-    this.apiService.post(this.apiBaseUrl,{id:1,title:"hey",completed:true}).subscribe(i => {
-      console.log(i);
-    })
-    this.apiService.post(this.apiBaseUrl,{id:1,title:"hey",completed:true},this.httpOptions).subscribe(i => {
-      console.log(i);
-    })
-
   }
 
   ngOnInit() {
