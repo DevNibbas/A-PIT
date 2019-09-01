ea4ec03fb41d7d1de92cec0c33c5a1db3f06da36
decrypt <yashtodkar2@gmail.com>
2019-09-02 01:40:32 +0530

    AutoGet completed.

--- src/app/api/autotest/autotest.component.ts
+++ src/app/api/autotest/autotest.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AutoTestGetService } from 'src/app/services/test/auto-test-get.service';
-import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-autotest',
@@ -10,19 +8,7 @@ import { HttpHeaders } from '@angular/common/http';
 
 export class AutotestComponent implements OnInit {
 
-  url:string = "https://jsonplaceholder.typicode.com/todos";
-  httpHeaders = new HttpHeaders({
-    'Content-Type' : 'application/json'
-  });
-  result:string[] = [];
-
-  constructor(private testService:AutoTestGetService) {
-    testService.testGet(this.url,this.httpHeaders).then(x=>{
-      x.forEach(element => {
-        console.log(element);
-      });
-    });
-  }
+  constructor() { }
 
   ngOnInit() {
 
--- src/app/services/test/auto-test-get.service.ts
+++ src/app/services/test/auto-test-get.service.ts
@@ -12,37 +12,30 @@ export class AutoTestGetService {
   httpOptions:any = {};
   ret:string[] = [];
   resp:any;
-  testcase1s:string = "Test without headers";
-  testcase2s:string = "Test with headers";
+  testcase1s:string = 'Test without headers';
+  testcase2s:string = 'Test with headers';
 
   constructor(private requestService:ApirequestService) { }
 
-  async testGet(url:string,httpHeaders:HttpHeaders):Promise<string[]>{
+  testGet(url: string, httpHeaders: HttpHeaders): Promise<string[]> {
     this.httpOptions.headers = httpHeaders;
-    // Testing without headers
-    console.log("Testing without headers.");
-    return await this.requestService.get(url).toPromise().then(resp => {
+    return this.requestService.get(url).toPromise().then(resp => {
       this.ret.push(`Testcase1:${this.testcase1s}:pass`);
-      console.log("Passed");
-    },resperr => {
+      console.log('Passed');
+    }, resperr => {
       this.ret.push(`Testcase1:${this.testcase1s}:fail`);
-      console.log("Failed.");
+      console.log('Failed.');
     }).then(resp => {
-    console.log("Testing with headers.");
-    return this.requestService.get(url,this.httpOptions).toPromise();
+      return this.requestService.get(url, this.httpOptions).toPromise();
     }).then(resp => {
-        this.ret.push(`Testcase2:${this.testcase2s}:pass`);
-        console.log("Passed");
-      },resperr => {
-        this.ret.push(`Testcase2:${this.testcase2s}:fail`);
-        console.log("Failed.");
-    }).then(x=>{
+      this.ret.push(`Testcase2:${this.testcase2s}:pass`);
+      console.log('Passed');
+    }, resperr => {
+      this.ret.push(`Testcase2:${this.testcase2s}:fail`);
+      console.log('Failed.');
+    }).then(x => {
       return this.ret;
     });
-
-  }
-
-  compl(){
 
   }
 
