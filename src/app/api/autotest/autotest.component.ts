7b4014c77f2c5c30f4480645e840fb3506216981
decrypt <yashtodkar2@gmail.com>
2019-09-03 23:09:31 +0530

    automated get and post services updated.

--- src/app/api/autotest/autotest.component.ts
+++ src/app/api/autotest/autotest.component.ts
@@ -1,6 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 import { AutoTestPostService } from './../../services/test/auto-test-post.service';
 import { Component, OnInit } from '@angular/core';
+import { AutoTestGetService } from 'src/app/services/test/auto-test-get.service';
 
 @Component({
   selector: 'app-autotest',
@@ -10,9 +11,12 @@ import { Component, OnInit } from '@angular/core';
 
 export class AutotestComponent implements OnInit {
 
-  constructor(private autoPost:AutoTestPostService) {
-    autoPost.testPost('https://jsonplaceholder.typicode.com/todos',['id','title','completed'],['^[0-9]{1-3}$',
-  '^[a-zA-Z]+$','^[a-z]+$'],new HttpHeaders({'Content-Type':'application/json'}),['id','title'],'id','201');
+  constructor(private autoPost:AutoTestPostService,
+    private autoGet:AutoTestGetService) {
+    console.log(autoPost.testPost('https://jsonplaceholder.typicode.com/todos',['id','title','completed'],['^[0-9]{1-3}$',
+  '^[a-zA-Z]+$','^[a-z]+$'],new HttpHeaders({'Content-Type':'application/json'}),['id','title'],'id','201'));
+  console.log(autoGet.testGet('https://jsonplaceholder.typicode.com/todos',
+  new HttpHeaders({'Content-Type':'application/json'})));
    }
 
   ngOnInit() {
--- src/app/services/test/auto-test-get.service.ts
+++ src/app/services/test/auto-test-get.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApirequestService } from 'src/app/api/apirequest.service';
 import { HttpHeaders } from '@angular/common/http';
+import { Observable, forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +12,36 @@ export class AutoTestGetService {
   httpOptions: any = {};
   ret: string[] = [];
   resp: any;
-  testcase1s = 'Test without headers';
-  testcase2s = 'Test with headers';
+  testcaseCount = 1;
+  url:string;
+  testcase1s = 'test without headers';
+  testcase2s = 'test with headers';
+  testcasesDetails:string[] = [this.testcase1s,this.testcase2s];
+  allReqs:Observable<any>[];
 
   constructor(private requestService: ApirequestService) { }
 
-  testGet(url: string, httpHeaders: HttpHeaders): Promise<string[]> {
+  testGet(url: string, httpHeaders: HttpHeaders):string[]{
+    this.testcaseCount = 1;
+    this.ret = [];
+    this.url = url;
     this.httpOptions.headers = httpHeaders;
+    this.allReqs = this.getAllReqs();
+    forkJoin(this.allReqs).subscribe(resp =>{
+      resp.forEach(x =>{
+        let i = resp.indexOf(x);
+        this.ret.push(`Testcase ${this.testcaseCount} : pass for ${this.testcasesDetails[i]}`);
+        this.testcaseCount += 1;
+      })
+    },err=>{
+      err.forEach(x => {
+        let i = err.indexOf(x);
+        this.ret.push(`Testcase ${this.testcaseCount} : fail for ${this.testcasesDetails[i]}`);
+        this.testcaseCount += 1;
+      });
+    });
+    return this.ret;
+    /*
     return this.requestService.get(url).toPromise().then(resp => {
       this.ret.push(`Testcase1:${this.testcase1s}:pass`);
       console.log('Passed');
@@ -35,7 +59,13 @@ export class AutoTestGetService {
     }).then(x => {
       return this.ret;
     });
+    */
+  }
 
+  getAllReqs():Observable<any>[]{
+    let woHeader = this.requestService.get(this.url);
+    let wHeader = this.requestService.get(this.url,this.httpOptions);
+    return [woHeader,wHeader];
   }
 
 }
