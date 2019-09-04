ceb701099970f339f13002a4431ddc6e67139e3b
decrypt <yashtodkar2@gmail.com>
2019-09-05 02:51:38 +0530

    Testing for a custom REST API

--- src/app/services/test/auto-test-post.service.ts
+++ src/app/services/test/auto-test-post.service.ts
@@ -1,3 +1,4 @@
+import { ApiTestingContractService } from './../common/api-testing-contract.service';
 import { StringGeneratorService } from './../common/string-generator.service';
 import { HttpHeaders } from '@angular/common/http';
 import { ApirequestService } from 'src/app/api/apirequest.service';
@@ -9,38 +10,30 @@ import { forkJoin, Observable } from 'rxjs';
 })
 export class AutoTestPostService {
 
-  httpOptions:any = {};
+  httpOptions:any = {observe:'response'};
   testcase_count:number = 0;
   url:string;
-  resp:any;
   ret:string[] = [];
-  params:any;
-  len:number;
   obs:Observable<any>[];
   trueVals:string[] = [];
-  resultVals:string[] = ["pass"];
   paramNames:string[] = [];
   paramRegex:string[] = [];
-  paramOptional:string[] = [];
   testResultParamNames = ["No"];
 
   constructor(private requestService:ApirequestService,
-    private stringGenService:StringGeneratorService) { }
+    private stringGenService:StringGeneratorService,
+    private apiContract:ApiTestingContractService) { }
 
-  testPost(url:string,paramNames:string[],paramRegex:string[],httpHeaders:HttpHeaders
-    ,paramOptional:string[],resultName:string,resultVal:string):string[]{
+  testPost(url:string,paramNames:string[],paramRegex:string[],httpHeaders:HttpHeaders):string[]{
     this.ret = [];
+    let passVals = [this.apiContract.HTTP_POST_SUCCESS_CODE,this.apiContract.HTTP_POST_CREATED_CODE];
+    let failVals = [this.apiContract.HTTP_POST_NO_CONTENT_CODE,this.apiContract.HTTP_POST_NOT_FOUND_CODE,this.apiContract.HTTP_POST_BAD_REQUEST];
     this.testcase_count = 1;
     this.url = url;
     this.httpOptions.headers = httpHeaders;
     this.paramNames = paramNames;
     this.paramRegex = paramRegex;
-    this.paramOptional = paramOptional;
     for(let index = 0; index<paramRegex.length;index++){
-      if(paramOptional.includes(paramNames[index]))
-        this.resultVals[index+1] = "pass";
-      else
-        this.resultVals[index+1] = "fail";
       this.trueVals.push(this.stringGenService.createString(paramRegex[index]));
       this.testResultParamNames.push(paramNames[index]);
     };
@@ -48,18 +41,18 @@ export class AutoTestPostService {
     forkJoin(this.obs).subscribe(resp =>{
       resp.forEach(x => {
         let i = this.testcase_count-1;
-        if(x[resultName]==resultVal)
-        if(this.resultVals[i]=='pass')
-        this.ret.push(`Testcase ${this.testcase_count} : pass for ${this.testResultParamNames[i]} parameter missing.`);
+        console.log(x);
+        console.log(passVals);
+        if(passVals.includes(x.status))
+        this.ret.push(`Testcase ${this.testcase_count} : pass for ${this.testResultParamNames[i]} parameter missing with code ${x.status}`);
         else
-        this.ret.push(`Testcase ${this.testcase_count} : fail for ${this.testResultParamNames[i]} parameter missing.`);
-        else
-        this.ret.push(`Testcase ${this.testcase_count} : fail for ${this.testResultParamNames[i]} parameter missing.`);
+        this.ret.push(`Testcase ${this.testcase_count} : fail for ${this.testResultParamNames[i]} parameter missing with code ${x.status}`);
         this.testcase_count += 1;
       })
     },err => {
+        console.log(err);
         let i = this.testcase_count - 1;
-        this.ret.push(`Testcase ${this.testcase_count} : fail for parameter ${this.testResultParamNames[i]} missing.`);
+        this.ret.push(`Testcase ${this.testcase_count} : fail for server error.`);
         this.testcase_count += 1;
     });
     return this.ret;
