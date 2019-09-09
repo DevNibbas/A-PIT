9a5ac7e7f44f4da84f8d8d87f4715f23947a1f1d
sammacorpy <sammacorpy@gmail.com>
2019-09-09 13:13:27 +0530

    fixed errors

--- src/app/crud.service.ts
+++ src/app/crud.service.ts
@@ -6,50 +6,52 @@ import { Injectable } from '@angular/core';
 })
 export class CrudService {
 
-  private _dbHistoryReq:IDBOpenDBRequest;
-  private _dbHistoryDB:IDBDatabase;
+  // tslint:disable-next-line:variable-name
+  private _dbHistoryReq: IDBOpenDBRequest;
+  // tslint:disable-next-line:variable-name
+  private _dbHistoryDB: IDBDatabase;
 
-  constructor() { 
+  constructor() {
     this._dbHistoryReq = window.indexedDB.open(HistIDBContract._dbNameHistory,
       HistIDBContract._dbVersionHistory);
-    this._dbHistoryReq.onupgradeneeded = ()=>{
+    this._dbHistoryReq.onupgradeneeded = () => {
       this._dbHistoryDB = this._dbHistoryReq.result;
-      let reqHist = this._dbHistoryDB.createObjectStore(
-        HistIDBContract._tReqHistoryName,{keyPath:HistIDBContract._tReqUniqueId,autoIncrement:true});
-      this.addIndexesWithOutOptions(reqHist,[HistIDBContract._tReqHistoryUserId,
-        HistIDBContract._tReqHistoryMethod,HistIDBContract._tReqHistoryURL,
-        HistIDBContract._tReqHistoryParams,HistIDBContract._tReqHistoryHeaders,
-        HistIDBContract._tReqHistoryData,HistIDBContract._tReqHistoryAuth,HistIDBContract._tReqHistoryAuthType,
-        HistIDBContract._tReqHistoryAuthUname,HistIDBContract._tReqHistoryAuthPwd,HistIDBContract._tReqHistoryBearerToken]);
+      const reqHist = this._dbHistoryDB.createObjectStore(
+        HistIDBContract._tReqHistoryName, { keyPath: HistIDBContract._tReqUniqueId, autoIncrement: true });
+      this.addIndexesWithOutOptions(reqHist, [HistIDBContract._tReqHistoryUserId,
+      HistIDBContract._tReqHistoryMethod, HistIDBContract._tReqHistoryURL,
+      HistIDBContract._tReqHistoryParams, HistIDBContract._tReqHistoryHeaders,
+      HistIDBContract._tReqHistoryData, HistIDBContract._tReqHistoryAuth, HistIDBContract._tReqHistoryAuthType,
+      HistIDBContract._tReqHistoryAuthUname, HistIDBContract._tReqHistoryAuthPwd, HistIDBContract._tReqHistoryBearerToken]);
     };
-    this._dbHistoryReq.onsuccess = ()=>{
+    this._dbHistoryReq.onsuccess = () => {
       this._dbHistoryDB = this._dbHistoryReq.result;
     };
   }
 
-  private addIndexesWithOutOptions(db:IDBObjectStore,indexNames:string[]):void{
+  private addIndexesWithOutOptions(db: IDBObjectStore, indexNames: string[]): void {
     indexNames.forEach(x => {
-      db.createIndex(x,x);
+      db.createIndex(x, x);
     });
   }
 
-  addRequestHistory(object:any){
-    let reqHistTransaction = this._dbHistoryDB.transaction(HistIDBContract._tReqHistoryName,
-        HistIDBContract._transactionRW);
+  addRequestHistory(object: any) {
+    const reqHistTransaction = this._dbHistoryDB.transaction(HistIDBContract._tReqHistoryName,
+      HistIDBContract._transactionRW);
     reqHistTransaction.objectStore(HistIDBContract._tReqHistoryName).put(object);
-    reqHistTransaction.oncomplete = ()=>{
+    reqHistTransaction.oncomplete = () => {
       console.log('Success');
-    }
-    reqHistTransaction.onerror = (err)=>{
+    };
+    reqHistTransaction.onerror = (err) => {
       console.log(err);
-    }
+    };
   }
 
-  getRequestHistory(uid:any):IDBRequest<any>{
-    let reqHistTransaction = this._dbHistoryDB.transaction(HistIDBContract._tReqHistoryName,
-        HistIDBContract._transactionRO);
-    let reqHistStore = reqHistTransaction.objectStore(HistIDBContract._tReqHistoryName);
-    let uidIndex = reqHistStore.index(HistIDBContract._tReqHistoryUserId);
+  getRequestHistory(uid: any): IDBRequest<any> {
+    const reqHistTransaction = this._dbHistoryDB.transaction(HistIDBContract._tReqHistoryName,
+      HistIDBContract._transactionRO);
+    const reqHistStore = reqHistTransaction.objectStore(HistIDBContract._tReqHistoryName);
+    const uidIndex = reqHistStore.index(HistIDBContract._tReqHistoryUserId);
     return uidIndex.getAll(uid);
   }
 
