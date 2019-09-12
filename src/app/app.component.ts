7304a14b82fbe2eb9c7480977f156bf9f57bbee0
sammacorpy <sammacorpy@gmail.com>
2019-09-12 17:12:57 +0530

    crud in app loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CrudService } from './crud.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'APIT';
+  constructor(private db: CrudService) {
+
+  }
 }
