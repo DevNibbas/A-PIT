fbf829a4551404c404ec6c1361859a6f0256b6eb
sammacorpy <sammacorpy@gmail.com>
2019-09-15 04:39:41 +0530

    environment ui modified

--- src/app/api/environment/environment.component.ts
+++ src/app/api/environment/environment.component.ts
@@ -11,25 +11,44 @@ export class EnvironmentComponent implements OnInit {
 
   json = JSON;
 
-  envs: any[] = [{ key: 'sample', value: 'this is a sample env declaration' } as any];
-
+  envs: any = { sample: 'this is a sample env declaration', sample2: 'this is a sample env declaration' } as any;
+  error;
+  sort = Array.so
   env: any = { key: '', value: '' };
+  object = Object;
+  envobjects;
   editEnv;
-  @ViewChild('inputkey', { static: false }) inpkey;
+  @ViewChild('#edit-env', { static: false }) inpkey;
 
-  constructor(private db: CrudService) { }
+  constructor(private db: CrudService) {
+    this.envobjects = this.object.keys(this.envs);
+    console.log(this.object.keys(this.envs));
+  }
 
   addEnv() {
-    this.envs.push({ key: this.env.key, value: this.env.value });
-    this.env = {};
+    if (this.env.key && !this.envs.hasOwnProperty(this.env.key)) {
+      this.envs[this.env.key] = this.env.value;
+      this.env = {};
+    } else if (this.env.key) {
+      this.error = 'Key already exist';
+    } else {
+      this.error = 'please enter a key';
+    }
     document.getElementById('inputkey').focus();
   }
 
   enableEdit(i) {
-    this.editEnv = this.envs[i];
+    this.editEnv = {};
+    this.editEnv.key = i;
+    this.editEnv.value = this.envs[i];
+    setTimeout(e => {
+      document.getElementById('edit-env-' + i).focus();
+
+    }, 500);
+
   }
   editEnvVar(i) {
-    this.envs[i] = this.editEnv;
+    this.envs[this.editEnv.key] = this.editEnv.value;
     this.editEnv = undefined;
   }
   deleteEnv(i) {
