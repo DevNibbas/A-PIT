fb07298750a21394a3b3702091a3fde8e7e98f69
sammacorpy <sammacorpy@gmail.com>
2019-09-23 17:11:06 +0530

    code filtering (removing unwanted codes)

--- src/app/api/environment/envparse.pipe.ts
+++ src/app/api/environment/envparse.pipe.ts
@@ -15,35 +15,18 @@ export class EnvparsePipe implements PipeTransform {
     if (!value) {
       return value;
     }
-    // return null;
     const y: any[] = value.match(/\{[a-zA-Z]+[a-zA-Z0-9]*\}/g);
 
     if (!y) {
       return value;
     }
-    // return new Promise((res, rej) => {
     // tslint:disable-next-line:prefer-for-of
     for (let e = 0; e < y.length; e++) {
       const key = y[e].replace(/[\{|\}]/g, '');
       const val = await this.env.parseEnv(key);
       value = value.replace(y[e], val);
-
-      // if (e === y.length - 1) {
-
-      //   res(value);
-      // }
-
-      // });
-
-
     }
-
-    // if (y.length === 0) {
     return value;
-    // }
-
-    // });
-
 
   }
 
